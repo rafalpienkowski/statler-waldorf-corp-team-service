5ac998467fc3c41a76a3b0647af5c83477162ee6
Rafal Pienkowski <rafal.pienkowski@cybercom.com>
2017-12-30 20:39:09 +0100

    Adding integration tests

---
+++ test/StatlerWaldorfCorp.TeamService.Tests.Integration/SimpleIntegrationTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using StatlerWaldorfCorp.TeamService.Models;
+using StatlerWaldorfCorp.TeamService;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatlerWaldorfCorp.TeamService.Tests.Integration
+{
+    public class SimpleIntegrationTests
+    {
+        private readonly TestServer _testServer;
+        private readonly HttpClient _testClient;
+        private readonly Team _teamZombie;
+
+        public SimpleIntegrationTests()
+        {
+            _testServer = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            _testClient = _testServer.CreateClient();
+            _teamZombie = new Team
+            {
+                Id = Guid.NewGuid(),
+                Name = "Zombie"
+            };
+        }
+
+        [Fact]
+        public async void TestTeamPostAndGet()
+        {
+            var stringContent = new StringContent(
+                JsonConvert.SerializeObject(_teamZombie),
+                UnicodeEncoding.UTF8,
+                "application/json"
+            );
+
+            var postMessage = await _testClient.PostAsync("/api/teams", stringContent);
+            postMessage.EnsureSuccessStatusCode();
+
+            var getMessage = await _testClient.GetAsync("/api/teams");
+            getMessage.EnsureSuccessStatusCode();
+
+            var raw = await getMessage.Content.ReadAsStringAsync();
+            var teams = JsonConvert.DeserializeObject<List<Team>>(raw);
+
+            Assert.Equal(3, teams.Count);
+            Assert.Equal("Zombie", teams[2].Name);
+            Assert.Equal(_teamZombie.Id, teams[2].Id);
+        }
+    }
+}
